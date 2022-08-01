e4546ce2b8a1d079987b54854c9f7c2a9cc193e4
jahanzeb jabbar <jahanzeb.jabbar@hotmail.com>
2022-08-01 21:39:00 +0800

    Base files added.

---
+++ src/main/java/com/bprocess/batchscheduler/SpringBatchSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.bprocess.batchscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchSchedulerApplication.class, args);
+    }
+
+}
