83f47cf737ef3c5194a20eadd037c678d5cb207f
jahanzeb jabbar <jahanzeb.jabbar@hotmail.com>
2022-08-04 10:00:52 +0800

    some modifications

--- src/main/java/com/bprocess/batchscheduler/services/ItemProcessService.java
+++ src/main/java/com/bprocess/batchscheduler/services/ItemProcessService.java
@@ -1,4 +1,17 @@
 package com.bprocess.batchscheduler.services;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class ItemProcessService {
+
+    public String checkVal(){
+
+        log.info("Item Processing Started ..");
+
+        return  "Done";
+    }
+
 }
